bcad4f3c8d68c78bf6834c6781a0437f6bd4a86b
Matt McKegg <matt@wetsand.co.nz>
2013-10-03 18:22:44 +1300

    added vibrato option to oscillator

--- sources/oscillator.js
+++ sources/oscillator.js
@@ -1,16 +1,22 @@
 module.exports = function(audioContext, source, at){
   // source: pitch, shape
   var oscillator = audioContext.createOscillator()
-  oscillator.frequency.value = getFrequency(source.pitch)
+  oscillator.detune.value = (source.pitch - 69) * 100
 
   if (source.shape){
     oscillator.type = source.shape
   }
 
+  if (source.vibrato){ // [freq, amp]
+    var sine = audioContext.createOscillator()
+    var amp = audioContext.createGain()
+    sine.start(at)
+    sine.frequency.value = source.vibrato[0] || 1
+    amp.gain.value = source.vibrato[1] || 0
+    sine.connect(amp)
+    amp.connect(oscillator.frequency)
+  }
+
   oscillator.start(at)
   return oscillator
-}
-
-function getFrequency(id){
-  return 440 * Math.pow(2, (id - 69.0) / 12.0)
 }
