f7647e2c5e0ef73ce1115332ebcec5e657067d9d
Matt McKegg <matt@wetsand.co.nz>
2014-01-16 23:38:40 +1300

    update adsr to support custom start and end values

--- modulators/adsr.js
+++ modulators/adsr.js
@@ -5,9 +5,15 @@ function ADSR(audioContext, descriptor, target){
     return new ADSR(audioContext, descriptor, target)
   }
   this.context = audioContext
-  this.descriptor = descriptor
+
+  this.update(descriptor)
   this._sustainFrom = null
   this.target = target
+}
+
+ADSR.prototype.sync = function(at){
+  this.target.cancelScheduledValues(at)
+  this.start(at)
 }
 
 ADSR.prototype.start = function(at){
@@ -15,7 +21,7 @@ ADSR.prototype.start = function(at){
   var attack = valueOrDefault(this.descriptor.attack, 0)
   var decay = valueOrDefault(this.descriptor.decay, 0)
   var sustain = valueOrDefault(this.descriptor.sustain, 1)
-  this.target.setValueAtTime(0, at)
+  this.target.setValueAtTime(this._start, at)
   this.target.linearRampToValueAtTime(multiplier, at+attack)
   this.target.linearRampToValueAtTime(sustain * multiplier, at+attack+decay)
   this._sustainFrom = at+attack+decay
@@ -29,7 +35,7 @@ ADSR.prototype.stop = function(at){
     if (at >= this._sustainFrom){
       this.target.setValueAtTime(sustain*multiplier, at)
     }
-    this.target.linearRampToValueAtTime(0, at+release)
+    this.target.linearRampToValueAtTime(this._end, at+release)
     this.target = null
     return at+release
   }
@@ -43,8 +49,17 @@ ADSR.prototype.disconnect = function(){
 }
 
 ADSR.prototype.update = function(descriptor){
+
+  if (!this.descriptor || descriptor.start != this.descriptor.start){
+    this._start = descriptor.start || 0
+  }
+
+  if (!this.descriptor || descriptor.end != this.descriptor.end){
+    this._end = descriptor.end || 0
+  }
+
   if (!this.descriptor || descriptor.sustain != this.descriptor.sustain){
-    var multiplier = valueOrDefault(this.descriptor.value, 1)
+    var multiplier = valueOrDefault(descriptor.value, 1)
     var sustain = valueOrDefault(descriptor.sustain, 1)
 
     if (this.target && this._sustainFrom && this.context.currentTime >= this._sustainFrom){
