1182c4a3555de820f69136682e566576f3766589
Matt McKegg <matt@wetsand.co.nz>
2014-01-28 14:35:33 +1300

    fix negative offsets with scale provider

--- providers/scale.js
+++ providers/scale.js
@@ -11,11 +11,15 @@ function getNote(scaleName, root, offset){
   var rootNote = typeof root === 'number' ? teoria.note.fromMIDI(root) : teoria.note(root)
   var scale = rootNote.scale(scaleName)
   var length = scale.scale.length
-  var position = offset % length
+  var position = mod(offset, length)
   var multiplier = Math.floor(offset/length)
   return getMIDI(scale.get(position + 1)) + (multiplier*12)
 }
 
 function getMIDI(note){
   return note.key() + 20
+}
+
+function mod(n, m) {
+  return ((n%m)+m)%m
 }
